9d0b6efa8af7d062f2063e905c1a2991809f0f4d
Sai Prasad Padhy <Saiprasadpadhy@gmail.com>
2021-04-04 23:52:54 +0530

    Tasks management app -- Initial Version

---
+++ app.js
@@ -0,0 +1,141 @@
+const form = document.querySelector('#task-form')
+const taskList = document.querySelector('.collection')
+const clearBtn = document.querySelector('.clear-tasks')
+const filter = document.querySelector('#filter')
+const taskInput = document.querySelector('#task')
+
+
+// Local Storage
+const setLocalStorage = (key, value) =>{
+    value = JSON.stringify(value);
+    localStorage.setItem(key, value);
+}
+
+const clearLocalStorage = () =>{
+    localStorage.clear();
+}
+
+const getLocalStorage = (key)=>{
+    return JSON.parse(localStorage.getItem(key));
+}
+
+const addTask = (e) => {
+    if(taskInput.value === '')
+    {
+        alert('Add Task')
+    }
+    else
+    {
+        const li = document.createElement('li');
+        li.className = 'collection-item';
+        // Create text node and append it to li
+        li.appendChild(document.createTextNode(taskInput.value));
+
+        const link = document.createElement('a');
+        link.className = 'delete-item secondary-content';
+
+        link.innerHTML = '<i class="fa fa-remove"></i>';
+        li.appendChild(link);
+
+        taskList.appendChild(li);
+
+        //Local Storage
+        let tasks;
+        if(getLocalStorage('tasks') == null){
+            tasks = [];
+        }
+        else{
+            tasks = getLocalStorage('tasks')
+        }
+
+        tasks.push(taskInput.value)
+
+        setLocalStorage('tasks', tasks)
+
+        taskInput.value ='';
+    }
+    e.preventDefault();
+}
+
+const removeTask = (e) =>{
+    //console.log(e.target)
+    // Event Delegation --- targetting the "delete-item" class
+    if(e.target.parentElement.classList.contains('delete-item')){
+        if(confirm('Are you sure you want to delete')){
+            e.target.parentElement.parentElement.remove();
+            //Remove from Local Storage
+            let taskToRemove = e.target.parentElement.parentElement.textContent;
+            let tasks = getLocalStorage('tasks')
+            let t = tasks.forEach((task, index)=>{
+                if(taskToRemove === task){
+                    tasks.splice(index, 1);
+                }
+            })
+            setLocalStorage('tasks', tasks)
+        }
+    }
+}
+
+const clearTasks = ()=>{
+    //taskList.innerHTML = '';
+    //Or
+    //https://www.measurethat.net/Benchmarks/Show/34/0/innerhtml-vs-removechild
+    while(taskList.firstChild){
+        taskList.removeChild(taskList.firstChild);
+    }
+    clearLocalStorage();
+}
+
+const filterTasks = (e)=> {
+    const typedtext = e.target.value.toLowerCase();
+
+    document.querySelectorAll('.collection-item').forEach((task)=> {
+        const item = task.firstChild.textContent;
+        if(item.toLowerCase().indexOf(typedtext) != -1){
+            task.style.display = 'block'
+        }
+        else{
+            task.style.display = 'none'
+        }
+    });
+}
+
+const getTasks = ()=>{
+    let tasks;
+    if(getLocalStorage('tasks') == null){
+        tasks = [];
+    }
+    else{
+        tasks = getLocalStorage('tasks')
+    }
+
+    tasks.forEach((task)=>{
+        const li = document.createElement('li');
+        li.className = 'collection-item';
+        // Create text node and append it to li
+        li.appendChild(document.createTextNode(task));
+
+        const link = document.createElement('a');
+        link.className = 'delete-item secondary-content';
+
+        link.innerHTML = '<i class="fa fa-remove"></i>';
+        li.appendChild(link);
+
+        taskList.appendChild(li);
+    })
+}
+
+
+const loadEventListeners = () => {
+    document.addEventListener('DOMContentLoaded',getTasks);
+
+    form.addEventListener('submit', addTask);
+
+    taskList.addEventListener('click',removeTask);
+
+    clearBtn.addEventListener('click',clearTasks);
+
+    filter.addEventListener('keyup',filterTasks);
+}
+
+loadEventListeners();
